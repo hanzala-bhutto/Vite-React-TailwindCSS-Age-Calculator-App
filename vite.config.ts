fab9ea3238002c815aeffbb8edb71e3a7293af58
hanzala-bhutto <bhutto.hanzala@gmail.com>
2023-10-31 12:59:54 +0500

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Vite-React-Tailwind-CSS-Template/"
+  base: "/Vite-React-TailwindCSS-Age-Calculator-App/"
 })
