f6d2d1b323af8e666f5a7918f3300a81cbe4c015
hanzala-bhutto <bhutto.hanzala@gmail.com>
2023-10-31 12:56:00 +0500

    Age calculator App setup styles font

---
+++ src/components/AgeForm.tsx
@@ -0,0 +1,10 @@
+
+const AgeForm =  () => {
+    return (
+        <>
+            <h1 className="font-bold">Age Form</h1>
+        </>
+    )
+}
+
+export default AgeForm;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'purple': 'hsl(259, 100%, 65%)',
+        'litRed': 'hsl(0, 100%, 67%)',
+        'offWhite': 'hsl(0, 0%, 94%)',
+        'litGrey': 'hsl(0, 0%, 86%)',
+        'smokeGrey':'hsl(0, 1%, 44%)',
+        'offBlack':'hsl(0, 0%, 8%)'
+      }
+    },
   },
   plugins: [],
 }
